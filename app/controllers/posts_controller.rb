7e1648826299ddb90906d5788124ebae64953fee
louis delon <louleman34@gmail.com>
2018-03-13 16:15:54 +0100

    fill in Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
+
+  before_action :post_params, only: [:show, :edit, :update]
+
   def index
+     @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
   end
 
   def show
@@ -15,5 +21,20 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update(post_params)
   end
+
+  private
+
+  def post_params
+    params.require(:post)
+      .permit(
+        :content,
+        :user_id,
+        :theme_id,
+        :category_id,
+        :group_id
+      )
+  end
+
 end
