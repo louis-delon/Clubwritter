f7e64505bddf9273a10e7fa9b1706a976d605beb
louis delon <louleman34@gmail.com>
2018-03-30 00:42:49 +0200

    fix bug, generate automatically a username when user regiters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_create :set_pseudo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -8,5 +10,11 @@ class User < ApplicationRecord
   has_many :inscriptions, dependent: :destroy
   has_many :themes, dependent: :destroy
   # validates :pseudo, presence: true, uniqueness: true
+
+  private
+
+  def set_pseudo
+    self.pseudo = "#{self.email[/^[^@]+/]}-#{SecureRandom.hex(1)}"
+  end
 
 end
