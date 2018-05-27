2e9536d85cad6e254d72bac33ff76e30dfcdf82b
LouisDelon <louleman34@gmail.com>
2018-05-28 00:02:57 +0200

    generate comment model and asociations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :theme
   belongs_to :category
+  has_many :comments
 
   validates :content, presence: true
   validates :theme, presence: true
