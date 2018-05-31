ebd5cad52fa6a0045f204564e10ea728ce7b4286
LouisDelon <louleman34@gmail.com>
2018-05-31 12:27:32 +0200

    try to fix bug of email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'louis@holdies.com'
+  config.mailer_sender = 'booby34_78@hotmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
